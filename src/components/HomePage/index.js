1716ada13009864d83d880890c9814e3b8a95ad6
BNThuyThao <39434133+thuythao1912@users.noreply.github.com>
2021-10-11 11:55:54 +0700

    Update index.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import { Button, Col, Row, Card } from "react-bootstrap";
 
 const HomePage = () => {
+  const [status, setStatus] = useState("high");
+  const [numStatus, setNumStatus] = useState(0);
+  const handleStatus = (e) => {
+    setStatus(e.target.value);
+  };
+  const handleNumStatus = (e) => {
+    if (e.target.value > 8 || e.target.value < 0) {
+      alert("Vui lòng nhập số từ 0 đến 8");
+      setNumStatus(0);
+    } else {
+      setNumStatus(e.target.value);
+    }
+  };
   return (
     <div className="container">
       <div className="input-group mb-3">
-        <select className="form-select form-select-sm">
-          <option>Nâng tông</option>
-          <option>Hạ tông</option>
+        <select className="form-select form-select-sm" onChange={handleStatus}>
+          <option value="high">Nâng tông</option>
+          <option value="low">Hạ tông</option>
         </select>
         <input
           type="number"
           className="form-control"
-          aria-label="Text input with dropdown button"
+          placeholder="Nhập số tông. VD: Đô -> Mi = 3"
+          onChange={handleNumStatus}
+          value={numStatus}
         />
       </div>
-
+      <span className="text-muted small">*Nhập số tông. VD: Đô - Mi = 3</span>
       <div className="my-3">
         <Row className="mx-0">
           <Button as={Col} variant="outline-primary">
@@ -118,10 +134,11 @@ const HomePage = () => {
           <img
             src="https://lh3.googleusercontent.com/proxy/iSBgDX7AYg4_mG9uC-oAJDyvuU28K6DQ3t748SZ_VyA1ng3JBdhgDPiMAoFIMA5EWIpJjOClHzXKmBP1WyyMBLfA68rIgVI"
             width="70%"
+            className="rounded-circle"
           />
         </Col>
         <Col className="text-center">
-          <Card border="primary" style={{ width: "100%" }}>
+          <Card border="success" style={{ width: "100%" }}>
             <Card.Body>
               <Card.Title>Sol</Card.Title>
             </Card.Body>
